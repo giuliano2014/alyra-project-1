8d5c0663b334bae5e395a24c3f84bb877b2bf194
Giuliano Furgol <giulianofurgol@hotmail.com>
2023-02-09 00:11:05 +0100

    Code refactoring and addition of checkpoints on various functions

--- Voting.sol
+++ Voting.sol
@@ -22,8 +22,19 @@ contract Voting is Ownable {
     // Mapping to store the state of the address in the whitelist
     mapping (address => bool) whitelist;
 
-    WorkflowStatus public votingStatus;
     Proposal[] public proposals;
+    WorkflowStatus public votingStatus;
+    uint256 public whitelistCount;
+
+    modifier isProposalsRegistrationStartedStatus(string memory _error) {
+        require(votingStatus == WorkflowStatus.ProposalsRegistrationStarted, _error);
+        _;
+    }
+
+    modifier isRegisteringVotersStatus(string memory _error) {
+        require(votingStatus == WorkflowStatus.RegisteringVoters, _error);
+        _;
+    }
 
     modifier onlyWhitelisted() {
         require(whitelist[msg.sender], "You are not authorized");
@@ -31,13 +42,12 @@ contract Voting is Ownable {
     }
 
     /**
-     * Add an address to the whitelist
-     * @dev Only the owner can call this function
-     * @param _address the address to add to the whitelist
+     * Add a proposal
+     * @dev Only the Whitelisted person can call this function
+     * @param _proposal the proposal description
      */
-    function setWhitelist(address _address) public onlyOwner {
-        require(votingStatus == WorkflowStatus.RegisteringVoters, "Voting is already launched");
-        whitelist[_address] = true;
+    function addProposal(string memory _proposal) public onlyWhitelisted isProposalsRegistrationStartedStatus("Right now, you can't add voting proposal") {
+        proposals.push(Proposal(_proposal, 0));
     }
 
     /**
@@ -50,22 +60,33 @@ contract Voting is Ownable {
         return whitelist[_address];
     }
 
-    function getVotingStatus() public view returns(WorkflowStatus) {
-        return votingStatus;
+    /**
+     * Add an address to the whitelist
+     * @dev Only the owner can call this function
+     * @param _address the address to add to the whitelist
+     */
+    function setWhitelist(address _address) public onlyOwner isRegisteringVotersStatus("It's too late to register new voters") {
+        whitelist[_address] = true;
+        whitelistCount++;
     }
 
-    function startProposalSession() public onlyOwner {
+    function startProposalSession() public onlyOwner isRegisteringVotersStatus("There are not enough subscribers in the whitelist") {
+        require(whitelistCount >= 2, "There are not enough subscribers in the whitelist");
         votingStatus = WorkflowStatus.ProposalsRegistrationStarted;
     }
 
-    /**
-     * Add a proposal
-     * @dev Only the Whitelisted person can call this function
-     * @param _proposal the proposal description
-     */
-    function addProposal(string memory _proposal) public onlyWhitelisted {
-        require(votingStatus == WorkflowStatus.ProposalsRegistrationStarted, "Voting is not yet launched");
-        proposals.push(Proposal(_proposal, 0));
+    function startVotingSession() public onlyOwner {
+        require(votingStatus == WorkflowStatus.ProposalsRegistrationEnded, "Right now, you can't start voting session");
+        votingStatus = WorkflowStatus.VotingSessionStarted;
+    }
+
+    function stopProposalSession() public onlyOwner isProposalsRegistrationStartedStatus("Right now, you can't stop proposal session") {
+        votingStatus = WorkflowStatus.ProposalsRegistrationEnded;
+    }
+
+    function voting() public view onlyWhitelisted returns(string memory) {
+        require(votingStatus == WorkflowStatus.VotingSessionStarted, "The vote session has not yet started");
+        return "Vote session is starting";
     }
 
 }
