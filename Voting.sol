b4e146c389150809e4b6050275e24a45fcb9571d
Giuliano Furgol <giulianofurgol@hotmail.com>
2023-02-08 12:23:16 +0100

    Add Ownable from OpenZeppelin to have an owner contract

--- Voting.sol
+++ Voting.sol
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-contract Voting {
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol"; 
+
+contract Voting is Ownable {
+
+    function isContractAdmin() public view onlyOwner returns(bool) {
+        return msg.sender == owner();
+    }
 
 }
