f2f656d6f0ac0dd39afd37512abe39438852dfe4
Giuliano Furgol <giulianofurgol@hotmail.com>
2023-02-13 14:35:48 +0100

    Add a voting factory

--- VotingFactory.sol
+++ VotingFactory.sol
@@ -5,8 +5,29 @@ import "./Voting.sol";
 
 contract VotingFactory {
 
-   function create() public returns (address) {
-       return address(new Voting());
-   }
-   
+    Voting public voting;
+
+    // mapping to store an array of voting contract addresses, keyed by client ID
+    mapping(string => address[]) private votingContractsByClientId;
+
+    /**
+     * Creates a new instance of the Voting contract and stores its address in the
+     * votingContractsByClientId mapping under the given client ID.
+     *
+     * @param _clientId the client ID to associate with the new contract instance
+     * @return the address of the newly created contract instance
+     */
+    function create(string calldata _clientId) public returns (address) {
+        // create a new instance of the Voting contract
+        address newVotingContract = address(new Voting());
+        // store the address of the new instance in the mapping, under the given client ID
+        votingContractsByClientId[_clientId].push(newVotingContract);
+        // return the address of the new contract instance
+        return newVotingContract;
+    }
+
+    function getVotingContractsByClientId(string calldata _clientId) public view returns (address[] memory) {
+        return votingContractsByClientId[_clientId];
+    }
+
 }
